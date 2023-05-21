b70e959aa3d4904ba5f74287b70a45db01844957
Neilansh Rajpal <neilanshjr@gmail.com>
2023-05-21 11:38:37 -0700

    created helper function to add user to firestore; checked error codes in signUpUser()

--- src/backend/helpers.js
+++ src/backend/helpers.js
@@ -2,22 +2,62 @@
 
 // import firebase-related stuff
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { collection, addDoc } from 'firebase/firestore';
+import { doc, setDoc, addDoc, collection } from 'firebase/firestore';
 
 import { auth, database } from './firebase';
 
 // function to sign UP with email and password
-// and add user details to DB
 // function returns errorMsg (if any)
-export const signUpUser = async (username, email, password, contactNumber) => {
-    console.log(username);
+export async function signUpUser (username, email, password, contactNumber) {
+    let errorMsg = '';
 
-   createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-    console.log('created.');
-   }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(error.message);
-});
+   createUserWithEmailAndPassword(auth, email, password).then(() => {
+        // custom function to add user details to firestore
+        errorMsg = addUser(username, email, contactNumber);
+        return errorMsg;
+    }).catch((error) => {
+        // catching different types of errors
+        switch(error.code){
+            case 'auth/email-already-in-use' : {
+                errorMsg = 'Email already exists. Please log in.';
+                break;
+            }
 
+            case 'auth/weak-password' : {
+                errorMsg = 'Weak password. Please use a stronger password.';
+                break;
+            }
+
+            case 'auth/timeout' : {
+                errorMsg = 'Your session timed out. Please try again.';
+                break;
+            }
+
+            default : {
+                errorMsg = 'An unknown error occurred. Please try again.'
+            }
+        }
+
+        //console.log(errorMsg);
+        return errorMsg;
+    });
+
+    
+}
+
+// function to ADD user to the database (firestore)
+export async function addUser(username, email, contactNumber){
+    let errorMsg = '';
+
+    addDoc(collection(database, 'users'), {
+        username: username,
+        email: email,
+        contact: contactNumber
+    }).catch(e => {
+        console.log(e);
+        errorMsg = 'error occurred';
+    });
+
+    // return error message (empty if no error)
+    return errorMsg;
 }
