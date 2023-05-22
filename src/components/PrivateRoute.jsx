cfaacf9cf748fa47a51a696e4c6521c297d15a52
Neilansh Rajpal <neilanshjr@gmail.com>
2023-05-21 23:17:55 -0700

    created PublicRoute and PrivateComponents (testing pending); issue #8

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,43 @@
+// PrivateRoute Component built around react-router.
+/*
+ A bit similar to PublicRoute, with some differences:
+ - Gets currentUser and isLoading from useCurrentUser Context.
+
+ - If !currentUser (== null) AND isLoading = false (user is signed OUT), redirect to the Login page.
+ - If currentUser (!= null) AND isLoading = false, (user is signed IN), renders the "children" Component.
+ - If isLoading = true, renders a Loading spinner.
+
+ - Prevents users from accessing pages that require them to be signed-in.
+ - Used for PRIVATE pages, that REQUIRE the user to be signed-in: Homepage, Settings, etc.
+*/
+
+import React from 'react';
+import { Route, redirect } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+
+import { useCurrentUser } from '../context/currentUser.context';
+
+export const PrivateRoute = ({children, ...routeProps}) => {
+    // extract currentUser and isLoading from useCurrentUser
+    const { currentUser, isLoading } = useCurrentUser();
+
+    // display Loading Spinner if currentUser == null and isLoading = false
+    // means that the data is still loading
+    if(!currentUser && isLoading){
+        return <Spinner animation="border" color='#E65728' />;
+    }
+
+    // check if currentUser is actually defined
+    /* in other words, if the user's logged-in or not
+       since currentProfile gets a value when the user logs in.
+    */
+   // if not, then redirect to Login page
+   if(!currentUser && !isLoading){
+        return redirect('/login');  // redirect to the Login page
+   }
+
+   // this condition implies user if signed-IN (currentUser && isLoading = false)
+   // otherwise, render the children (wrapped by this Component)
+   // ONLY happens when the user is signed-in
+   return <Route {...routeProps}>{children}</Route>;
+}
---
+++ src/components/PublicRoute.jsx
@@ -0,0 +1,43 @@
+// PublicRouter Component built around react-router.
+/*
+ Works as a simple router, with additional functionalities:
+ - Gets currentUser and isLoading from useCurrentUser Context.
+
+ - If !currentUser (== null) AND isLoading = false (user is signed OUT), renders the "children" Component.
+ - If currentUser (!= null) AND isLoading = false, (user is signed IN), redirect them to homepage.
+ - Else, renders a Loading spinner.
+
+ - Just a clean way or rendering pages.
+ - Used for public pages, that don't require the user to be logged-in: Signup, Login, ForgotPassword.
+*/
+
+import React from 'react';
+import { Route, redirect } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+
+import { useCurrentUser } from '../context/currentUser.context';
+
+export const PublicRoute = ({children, ...routeProps}) => {
+    // extract currentUser and isLoading from useCurrentUser
+    const { currentUser, isLoading } = useCurrentUser();
+
+    // display Loading Spinner if currentUser == null and isLoading = false
+    // means that the data is still loading
+    if(!currentUser && isLoading){
+        return <Spinner animation="border" color='#E65728' />;
+    }
+
+    /*
+     since this Component will only be used for Signup, Login and ForgotPassword,
+     we'll only direct them to those pages if they're signed-OUT (check last condition).
+     If they're signed in, direct them to Homepage by default.
+    */
+   if(currentUser && !isLoading){
+        return redirect('/');  // redirect to homepage
+   }
+
+    // this condition implies user if signed-IN (currentUser && isLoading = false)
+   // otherwise, user is signed OUT, so render the children (wrapped by this Component) 
+   // In this case, children is ONLY one of Signup, Login or ForgotPassword
+   return <Route {...routeProps}>{children}</Route>;
+}
